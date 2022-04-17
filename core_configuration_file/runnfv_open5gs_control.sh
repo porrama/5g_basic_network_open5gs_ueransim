8287b7b36e1b692ddbc5d05d3983031709a2bad2
Porrama Rakchaithanakorn <porrama.pee@gmail.com>
2022-04-17 21:40:14 +0700

    Create runnfv_open5gs_control.sh

---
+++ core_configuration_file/runnfv_open5gs_control.sh
@@ -0,0 +1,13 @@
+# Mongodb
+systemctl start mongodb
+
+# Network Function
+./open5gs-nrfd &
+./open5gs-smfd &
+./open5gs-amfd &
+./open5gs-ausfd &
+./open5gs-udmd &
+./open5gs-udrd &
+./open5gs-pcfd &
+./open5gs-nssfd &
+./open5gs-bsfd 
